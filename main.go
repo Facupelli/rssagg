200539ffcdf601b01f9f7d18ba5e011ba83b1d51
Facundo Personal <facundopellicer4@gmail.com>
2024-08-21 19:47:56 -0300

    boilerplate for http server

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://*", "https://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300, 
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handleReadiness)
+	v1Router.Get("/error", handleError)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %s", portString)
+	err :=server.ListenAndServe() // ListenAndServe blocks
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PORT:", portString)
+}
